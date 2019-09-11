40ee1fa8c888fbaf37acadcaee5a6a6c52d8f6c1
hm <hmcpherson@ischool.berkeley.edu>
2019-09-11 00:08:07 -0700

    adding apache and php docker containers

---
+++ app/my-app/public_html/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>just-add-water</title>
+</head>
+<body>
+<h1>just-add-water</h1>
+<?php
+	/*
+	echo "<h4>Attempting MySQL connection from php...</h4>";
+	$host = 'mysql';
+	$user = 'root';
+	$pass = 'rootpassword';
+	$conn = new mysqli($host, $user, $pass);
+
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	} else {
+	    echo "Connected to MySQL successfully!";
+	}
+	*/
+?>
+</body>
+</html>
