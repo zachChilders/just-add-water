ebdf370208cef410c92e83eb239d86eab903dd10
Zach <zwc101@gmail.com>
2019-09-19 06:17:18 +0000

    readded docker

--- Deploy-Infrastructure.ps1
+++ Deploy-Infrastructure.ps1
@@ -6,127 +6,133 @@ $kv_name = "mics-kv"
 
 # Bootstrap Requirements
 if (-not (Get-InstalledModule  Requirements)) {
-  Install-Module Requirements -Force
+    Install-Module Requirements -Force
 }
 Import-Module Requirements
 
 # Auth Azure and gather subscription secrets
 $azureReqs = @(
-  @{
-    Name     = "Azure Login"
-    Describe = "Authenticate Azure Session"
-    Test     = {
-      (az account show | ConvertFrom-Json).state -eq "Enabled"
+    @{
+        Name     = "Azure Login"
+        Describe = "Authenticate Azure Session"
+        Test     = {
+            (az account show | ConvertFrom-Json).state -eq "Enabled"
+        }
+        Set      = { az login }
+    },
+    @{ # This could be done idempotently with a test,
+        # but refreshing the secrets every run allows for
+        # new secrets to be added easily
+        Name     = "Keyvault Secrets"
+        Describe = "Inject Secrets into Session"
+        Set      = {
+            $KEYVAULTNAME = $kv_name
+            $SECRETS = ( $(az keyvault secret list --vault-name $KEYVAULTNAME | jq '.[].id' -r | sed 's/.*\/\([^/]\+\)$/\1/') )
+            $SECRETS | % {
+                $SECRET = $(az keyvault secret show --name $_ --vault-name $KEYVAULTNAME | jq '.value' -r)
+                $NAME = $_.Replace("-", "_")
+                [Environment]::SetEnvironmentVariable($NAME, $SECRET)
+            }
+        }
     }
-    Set      = { az login }
-  },
-  @{ # This could be done idempotently with a test,
-    # but refreshing the secrets every run allows for
-    # new secrets to be added easily
-    Name     = "Keyvault Secrets"
-    Describe = "Inject Secrets into Session"
-    Set      = {
-      $KEYVAULTNAME = $kv_name
-      $SECRETS = ( $(az keyvault secret list --vault-name $KEYVAULTNAME | jq '.[].id' -r | sed 's/.*\/\([^/]\+\)$/\1/') )
-      $SECRETS | % {
-        $SECRET = $(az keyvault secret show --name $_ --vault-name $KEYVAULTNAME | jq '.value' -r)
-        $NAME = $_.Replace("-", "_")
-        [Environment]::SetEnvironmentVariable($NAME, $SECRET)
-      }
-    }
-  }
 )
 
 # Provision Infra
 $provisionReqs = @(
-  @{
-    Name     = "Transform Environment Variables"
-    Describe = "Transform Terraform Variables"
-    Test     = {
-      $env:TF_VAR_client_id.Length -gt 0 -and $env:TF_VAR_client_secret.Length -gt 0
+    @{
+        Name     = "Transform Environment Variables"
+        Describe = "Transform Terraform Variables"
+        Test     = {
+            $env:TF_VAR_client_id.Length -gt 0 -and $env:TF_VAR_client_secret.Length -gt 0
+        }
+        Set      = {
+            $env:TF_VAR_client_id = $env:ARM_CLIENT_ID
+            $env:TF_VAR_client_secret = $env:ARM_CLIENT_SECRET
+        }
+    },
+    @{
+        Name     = "Terraform init"
+        Describe = "Initialize terraform environment"
+        Test     = { Test-Path "$PSScriptRoot/tf/.terraform" }
+        Set      = {
+            Set-Location -Path "tf"
+            terraform init -backend-config="storage_account_name=$($tf_share)" -backend-config="container_name=tfstate" -backend-config="access_key=$($env:terraform_storage_key)" -backend-config="key=mics.tfstate"
+        }
+    },
+    @{
+        Name     = "Terraform plan"
+        Describe = "Plan terraform environment"
+        Test     = { Test-Path "$PSScriptRoot/out/out.plan" }
+        Set      = {
+            New-Item -Path "$PSScriptRoot/out" -ItemType Directory -Force
+            terraform plan -out "$PSScriptRoot/out/out.plan" | Write-Output
+        }
+    },
+    @{
+        Name     = "Terraform Apply"
+        Describe = "Apply Terraform plan"
+        Test     = { Test-Path "./out/azurek8s" } # TODO: probe infra for test
+        Set      = {
+            terraform apply "../out/out.plan" | Write-Information
+            terraform output kube_config | Out-File ../out/azurek8s
+            Set-Location -Path ".."
+        }
     }
-    Set      = {
-      $env:TF_VAR_client_id = $env:ARM_CLIENT_ID
-      $env:TF_VAR_client_secret = $env:ARM_CLIENT_SECRET
-    }
-  },
-  @{
-    Name     = "Terraform init"
-    Describe = "Initialize terraform environment"
-    Test     = { Test-Path "$PSScriptRoot/tf/.terraform" }
-    Set      = {
-      Set-Location -Path "tf"
-      terraform init -backend-config="storage_account_name=$($tf_share)" -backend-config="container_name=tfstate" -backend-config="access_key=$($env:terraform_storage_key)" -backend-config="key=mics.tfstate"
-    }
-  },
-  @{
-    Name     = "Terraform plan"
-    Describe = "Plan terraform environment"
-    Test     = { Test-Path "$PSScriptRoot/out/out.plan" }
-    Set      = {
-      New-Item -Path "$PSScriptRoot/out" -ItemType Directory -Force
-      terraform plan -out "$PSScriptRoot/out/out.plan" | Write-Output
-    }
-  },
-  @{
-    Name     = "Terraform Apply"
-    Describe = "Apply Terraform plan"
-    Test     = { Test-Path "./out/azurek8s" } # TODO: probe infra for test
-    Set      = {
-      terraform apply "../out/out.plan" | Write-Information
-      terraform output kube_config | Out-File ../out/azurek8s
-      Set-Location -Path ".."
-    }
-  }
 )
 
-# TODO: Put docker in docker so you can docker while you docker
 # Docker cooking
-# $dockerReqs = @(
-#   @{
-#     Name   = "Find Docker Services"
-#     Describe = "Enumerate Containers"
-#     Set    = {
-#       $services = Get-ChildItem ./app
-#     }
-#   },
-#   @{
-#     Name   = "Build Docker Containers"
-#     Describe = "Build Docker"
-#     Set    = {
-#       $services = Get-ChildItem ./app
-#       Write-Host $services
-#     }
-#   }
-# )
+$dockerReqs = @(
+    @{
+        Name     = "Find Docker Services"
+        Describe = "Enumerate Containers"
+        Set      = {
+            ./scripts/Set-k8sConfig.ps1
+        }
+    },
+    @{
+        Name     = "Build Docker Containers"
+        Describe = "Build all containers"
+        Set      = {
+            $list = Get-ChildItem ./out/k8s.json | ConvertFrom-Json
+            $list | % { docker build -t $_.Name $_.Path }
+        }
+    },
+    @{
+        Name     = "Push Containers"
+        Describe = "Push all containers"
+        Set      = {
+            az acr push
+        }
+    }
+)
 
 # Kubernetes Deployment
 $k8sReqs = @(
-  @{
-    Name     = "Load Config"
-    Describe = "Load k8s config"
-    Set      = {
-      $env:KUBECONFIG = "./out/azurek8s"
+    @{
+        Name     = "Load Config"
+        Describe = "Load k8s config"
+        Set      = {
+            $env:KUBECONFIG = "./out/azurek8s"
+        }
+    },
+    @{
+        Name     = "Deploy Application"
+        Describe = "Application deployment"
+        Set      = {
+            kubectl apply -f pod.yml
+        }
+    },
+    @{
+        Name     = "Set autoscale"
+        Describe = "Configure Autoscale"
+        Test     = { (kubectl get hpa).length -gt 1 }
+        Set      = {
+            kubectl autoscale deployment mics-test --min=2 --max=5 --cpu-percent=80
+        }
     }
-  },
-  @{
-    Name     = "Deploy Application"
-    Describe = "Application deployment"
-    Set      = {
-      kubectl apply -f pod.yml
-    }
-  },
-  @{
-    Name     = "Set autoscale"
-    Describe = "Configure Autoscale"
-    Test     = { (kubectl get hpa).length -gt 1 }
-    Set      = {
-      kubectl autoscale deployment mics-test --min=2 --max=5 --cpu-percent=80
-    }
-  }
 )
 
 $azureReqs | Invoke-Requirement | Format-Checklist
 $provisionReqs | Invoke-Requirement | Format-Checklist
-# $dockerReqs | Invoke-Requirement | Format-Checklist
+$dockerReqs | Invoke-Requirement | Format-Checklist
 $k8sReqs | Invoke-Requirement | Format-Checklist
--- scripts/Set-k8sConfig.ps1
+++ scripts/Set-k8sConfig.ps1
@@ -1,49 +1,39 @@
-Param(
-  [ValidateScript( { Test-Path $_ } )]
-  [string]$Path
-)
-
 $ErrorActionPreference = "Stop"
-
-$OutPath = "$Path/out"
 
 # Class to hold Docker structure
 class Docker {
-  [String] $Name
-  [String] $Path
-  [Hashtable] $Commands
-  [Boolean] $Frontend # To identify which port serves traffic
+    [String] $Name
+    [String] $Path
+    [Hashtable] $Commands
+    [Boolean] $Frontend # To identify which port serves traffic
 }
 
-# create output path
-$OutPath `
-| ? { -not (Test-Path $_) } `
-| % { New-Item $_ -ItemType Directory }
+$Path = "out"
 
 # parse each Dockerfile in to a K8S JSON
-Get-ChildItem $Path -Filter "Dockerfile" -Recurse `
+Get-ChildItem -Filter "Dockerfile" -Recurse `
 | % {
-  # parse each Dockerfile directive into @{$command => $args}
-  $commands = @{ }
-
-  Get-Content $_.FullName `
+    # parse each Dockerfile directive into @{$command => $args}
+    $commands = @{ }
+    Get-Content $_.FullName `
   | ? { $_ -notmatch "^\s*$" } `
   | % {
-    # First word is cmd, all else is args
-    $words = $_ -split " "
-    $cmd = $words | Select -First 1
-    $args = $words | Select -Skip 1
+        # First word is cmd, all else is args
+        $words = $_ -split " "
+        $cmd = $words | Select -First 1
+        $args = $words | Select -Skip 1
 
-    # set/append args in the command hash
-    $commands[$cmd] += $args
-  }
-
-  [Docker] @{
-    Path     = $_.FullName
-    Name     = $_.FullName.split("/")[-2]
-    Commands = $commands
-    Frontend = $false
-  }
+        # set/append args in the command hash
+        if (-not [String]::IsNullOrEmpty($cmd)) {
+            $commands[$cmd] += $args
+        }
+    }
+    [Docker] @{
+        Path     = $_.DirectoryName
+        Name     = $_.FullName.split("/")[-2]
+        Commands = $commands
+        Frontend = $false
+    }
 } `
 | ConvertTo-Json `
-| Out-File -FilePath "$OutPath/k8s.json" -Force
+| Out-File "$Path/k8s.json"
