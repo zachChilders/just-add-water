c0f48ffd30294a28d0b0d37697639269239ae0e9
Zach <zwc101@gmail.com>
2019-09-22 01:09:54 -0700

    fixing purposeful lint error

--- tf/k8s.tf
+++ tf/k8s.tf
@@ -10,7 +10,7 @@ resource "azurerm_log_analytics_workspace" "law" {
   sku                 = "${var.log_analytics_workspace_sku}"
 }
 
-resoasdfurce "azurerm_log_analytics_solution" "las" {
+resource "azurerm_log_analytics_solution" "las" {
   solution_name         = "ContainerInsights"
   location              = "${azurerm_log_analytics_workspace.law.location}"
   resource_group_name   = "${azurerm_resource_group.rg.name}"
