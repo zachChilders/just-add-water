82dce6af0fb921e08ebc5c0268e21a83951a3bc3
Zach <zwc101@gmail.com>
2019-09-24 00:03:42 -0700

    pr feedback

--- modules/jaw/jaw.psm1
+++ modules/jaw/jaw.psm1
@@ -2,11 +2,11 @@ $ErrorActionPreference = "Stop"
 
 # Class to hold Docker structure
 class Docker {
-    [string] $Name
-    [string] $ImageName
-    [string] $Path
-    [string[]] $Ports
-    [boolean] $Frontend # To identify which port serves traffic
+  [string] $Name
+  [string] $ImageName
+  [string] $Path
+  [string[]] $Ports
+  [boolean] $Frontend # To identify which port serves traffic
 }
 
 <#
@@ -16,19 +16,19 @@ class Docker {
   Works on any type of config.  Replaces "{{varname}}" => $varname.
 #>
 function Expand-Template {
-    [CmdletBinding()]
-    [OutputType([String])]
-    Param(
-        [Parameter(Mandatory)]
-        [ValidateNotNullOrEmpty()]
-        [string] $Template,
-        [Parameter(Mandatory)]
-        [hashtable] $Data
-    )
+  [CmdletBinding()]
+  [OutputType([String])]
+  Param(
+    [Parameter(Mandatory)]
+    [ValidateNotNullOrEmpty()]
+    [string] $Template,
+    [Parameter(Mandatory)]
+    [hashtable] $Data
+  )
 
-    $instance = $Template
-    $data.keys | % { $instance = $instance -replace "{{$_}}", $Data[$_] }
-    $instance
+  $instance = $Template
+  $data.keys | % { $instance = $instance -replace "{{$_}}", $Data[$_] }
+  $instance
 }
 
 <#
@@ -38,29 +38,34 @@ function Expand-Template {
   Searches for dockerfiles and saves information about them for later use.
 #>
 function Set-k8sConfig {
-    [CmdletBinding(SupportsShouldProcess = $true)]
-    param(
-        [string] $AppPath,
-        [string] $OutPath
-    )
-    # parse each Dockerfile in to a K8S JSON
-    # we only check for top level dockerfiles right now.
-    Get-ChildItem -Path $AppPath -Filter "*dockerfile*" -Recurse `
-    | % {
-        $Name = $_.FullName
-        $Path = $_.Directory
-        $ImageName = ($Path -split "/")[-1]
-        $Ports = docker inspect mics233.azurecr.io/$ImageName --format="{{json .Config}}"
-        $Ports = ($Ports | ConvertFrom-Json -AsHashtable).ExposedPorts.Keys | % { ($_ -split "/")[0] }
+  [CmdletBinding(SupportsShouldProcess = $true)]
+  param(
+    [string] $AppPath,
+    [string] $OutPath
+  )
+  # parse each Dockerfile in to a K8S JSON
+  # we only check for top level dockerfiles right now.
+  Get-ChildItem -Path $AppPath -Filter "*dockerfile*" -Recurse `
+  | % {
+    # Full File Name
+    $Name = $_.FullName
+    # Parent Directory
+    $Path = $_.Directory
+    # This resolves to the folder directly beneath /app
+    $ImageName = ($Path -split "/")[-1]
+    # Query inactive container for config
+    $Config = docker inspect mics233.azurecr.io/$ImageName --format="{{json .Config}}"
+    # Extract port numbers out of config
+    $Ports = ($Config | ConvertFrom-Json -AsHashtable).ExposedPorts.Keys | % { ($_ -split "/")[0] }
 
-        [Docker] @{
-            Name      = $Name
-            ImageName = $ImageName
-            Path      = $Path
-            Ports     = $Ports
-            Frontend  = $false
-        }
-    } `
-    | ConvertTo-Json `
-    | Out-File "$OutPath/k8s.json"
+    [Docker] @{
+      Name      = $Name
+      ImageName = $ImageName
+      Path      = $Path
+      Ports     = $Ports
+      Frontend  = $false
+    }
+  } `
+  | ConvertTo-Json `
+  | Out-File "$OutPath/k8s.json"
 }
