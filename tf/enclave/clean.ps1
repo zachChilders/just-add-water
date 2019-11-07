372964446a6334cddab0c849328d03de66613150
Zach Childers <zachilde@microsoft.com>
2019-11-07 06:17:23 +0000

    better clean script

--- tf/enclave/clean.ps1
+++ tf/enclave/clean.ps1
@@ -3,24 +3,28 @@ $OutDir = "$RepoRoot/out"
 
 # Delete k8s Resource Group
 $azResourceGroups = (az group list) | ConvertFrom-Json
-$azName = ($azResourceGroups | ? {$_.Name -like "sbd"}).name
+$azName = ($azResourceGroups | ? { $_.Name -like "sbd" }).name
 az group delete --name $azName -y
 
 
 # Delete Infrastructure Resource Group
 $azResourceGroups = (az group list) | ConvertFrom-Json
-$azName = ($azResourceGroups | ? {$_.Name -like "MC_sbd_sbd_southcentralus"}).name
+$azName = ($azResourceGroups | ? { $_.Name -like "MC_sbd_sbd_southcentralus" }).name
 az group delete --name $azName -y
 
-# Remove Terraform State
-rm -rf ./.terraform
-rm terraform.tfstate
-rm terraform.tfstate.backup
+# Delete TM Endpoint
+az network traffic-manager endpoint delete -g "sbd-global" --profile-name "sbd-atm" --type "Microsoft.Network/trafficManagerProfiles/azureEndpoints" --name "mics-sbd"
 
 # Delete just-add-water files
-rm $OutDir/azurek8s
-rm $OutDir/out.plan
-rm $OutDir/k8s.json
-rm $OutDir/pod.yml
+Remove-Item -Recurse ".terraform" -Force
+
+"terraform.tfstate",
+"terraform.tfstate.backup",
+"$OutDir/azurek8s,"
+"$OutDir/out.plan,"
+"$OutDir/k8s.json,"
+"$OutDir/pod.yml" | % {
+    Remove-Item $_ | Out-Null
+}
 
 Set-Location $RepoRoot
