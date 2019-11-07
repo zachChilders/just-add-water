4f086b7997732d6f1a2fcff8c07a6bb083cb4117
Zach Childers <zachilde@microsoft.com>
2019-11-07 03:55:08 +0000

    added tests

--- .test/scripts/sbd.tests.ps1
+++ .test/scripts/sbd.tests.ps1
@@ -4,13 +4,22 @@ $OutRoot = "$ScriptsRoot/../out"
 
 Describe "Validate Deploy.ps1" {
     BeforeAll {
+        git submodule update --init
         & "$RepoRoot/deploy.ps1"
     }
     AfterAll {
         & "$RepoRoot/tf/enclave/clean.ps1"
     }
-    It "Created Azure Resources" {}
-    It "Renders an Actual Page" {}
-    It "Can Connect to Resources" {}
-    It "Ran All Hardening Scripts" {}
+    It "Created Azure Resources" {
+        az group exists --name sbd | Should -Be "true"
+    }
+    It "Renders an Actual Page" {
+        (Invoke-WebRequest sbd.trafficmanager.net).StatusCode | Should -Be 200
+    }
+    It "Can Connect to Resources" {
+        [boolean] (kubectl get deployment) | Should -Be $true
+    }
+    It "Ran All Hardening Scripts" {
+        $true
+    }
 }
