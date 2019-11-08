299941dfbd5fea7ece578450d992dede91b15dda
Zach Childers <zachilde@microsoft.com>
2019-11-08 17:41:10 +0000

    removing file cleanup steps entirely

--- tf/enclave/clean.ps1
+++ tf/enclave/clean.ps1
@@ -2,9 +2,6 @@ param(
     [string]$EnclaveName = "sbd"
 )
 
-$RepoRoot = "$PSScriptRoot/../.."
-$OutDir = "$RepoRoot/out"
-
 # Delete k8s Resource Group
 az group delete --name $EnclaveName -y
 
@@ -14,14 +11,5 @@ az group delete --name "MC_$($EnclaveName)_$($EnclaveName)_southcentralus" -y
 # Delete TM Endpoint
 az network traffic-manager endpoint delete -g "sbd-global" --profile-name "sbd-atm" --type "azureEndpoints" --name $EnclaveName
 
-# Delete just-add-water files
-"$OutDir/azurek8s",
-"$OutDir/$EnclaveName.plan",
-"$OutDir/k8s.json",
-"$OutDir/pod.yml" | % {
-    Remove-Item $_ -Force | Out-Null
-}
-
-Set-Location $RepoRoot
 
 exit(0)
