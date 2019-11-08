3ec56a8e726e800fd1a3c918014e58a808898943
Zach Childers <zachilde@microsoft.com>
2019-11-08 07:42:35 +0000

    stupid errors

--- tf/enclave/clean.ps1
+++ tf/enclave/clean.ps1
@@ -8,7 +8,6 @@ $OutDir = "$RepoRoot/out"
 # Delete k8s Resource Group
 az group delete --name $EnclaveName -y
 
-
 # Delete Infrastructure Resource Group
 az group delete --name "MC_$($EnclaveName)_$($EnclaveName)_southcentralus" -y
 
@@ -16,9 +15,6 @@ az group delete --name "MC_$($EnclaveName)_$($EnclaveName)_southcentralus" -y
 az network traffic-manager endpoint delete -g "sbd-global" --profile-name "sbd-atm" --type "azureEndpoints" --name $EnclaveName
 
 # Delete just-add-water files
-
-"./terraform.tfstate",
-"./terraform.tfstate.backup",
 "$OutDir/azurek8s",
 "$OutDir/$EnclaveName.plan",
 "$OutDir/k8s.json",
