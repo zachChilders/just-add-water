de44bcdbd531654c2228ab209fa3d6344b9e1a5b
Zach Childers <526430+zachChilders@users.noreply.github.com>
2019-12-05 11:20:53 -0800

    HTTPS!! (#89)
    * HTTPS!!
    
    * fixing test
    
    * test fix

--- .test/scripts/sbd.tests.ps1
+++ .test/scripts/sbd.tests.ps1
@@ -1,10 +1,11 @@
 $RepoRoot = "$PSScriptRoot/../.."
 $EnclaveName = "micssbdtest$((([guid]::newguid().guid) -replace "-").substring(1,5))"
+$HostName = "$EnclaveName.southcentralus.cloudapp.azure.com"
 
 Describe "Validate Deploy.ps1" {
     BeforeAll {
         git submodule update --init
-        & "$RepoRoot/deploy.ps1" -EnclaveName $EnclaveName
+        & "$RepoRoot/deploy.ps1" -EnclaveName $EnclaveName -HostName $HostName
     }
     AfterAll {
         & "$RepoRoot/tf/enclave/clean.ps1" -EnclaveName $EnclaveName
@@ -14,7 +15,7 @@ Describe "Validate Deploy.ps1" {
     }
     It "Renders an Actual Page" {
         Start-Sleep -Seconds 300 # Ensure DNS has time to propagate
-        (Invoke-WebRequest "$($EnclaveName).southcentralus.cloudapp.azure.com").StatusCode | Should -Be 200
+        (Invoke-WebRequest $HostName -SkipCertificateCheck).StatusCode | Should -Be 200
     }
     It "Can Connect to Resources" {
         [boolean] (kubectl get deployment) | Should -BeTrue
